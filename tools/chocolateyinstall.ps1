a2c6d992c5aff8864ec241b0befdef236f0cd0fb
Tim Abell <tim@timwise.co.uk>
2015-08-23 00:17:19 +0100

    Don't specify visual studio version
    According to the choco docs this will mean it gets installed to
    the latest available visual studio.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $packageArgs = @{
   vsixUrl       = 'https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix'
   checksum      = 'f41c4d97ce6e5fc61f0242fc172f88e9'
   checksumType  = 'md5' #default is md5, can also be sha1
-  vsVersion     = 14 # vs2015
+  # vsVersion     = 14 # vs2015 - left out, by default will install to latest visual studio
 }
 Install-ChocolateyVsixPackage @packageArgs
